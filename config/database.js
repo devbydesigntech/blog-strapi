babc15db24a70fafc7f823608cdc9830b95e49d0
Chris <chrisliu0416@gmail.com>
2022-10-09 00:18:05 -0700

    commit

--- config/database.js
+++ config/database.js
@@ -2,14 +2,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '0000'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
       },
     },
     debug: false,
