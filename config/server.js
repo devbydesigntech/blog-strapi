68e05990eea3b195817ddb252250f6f1b748c332
Chris <chrisliu0416@gmail.com>
2022-10-09 00:42:55 -0700

    production server commit

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
-  app: { 
-    keys: env.array('APP_KEYS')
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
